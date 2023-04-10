fb87dd56ed428a665c9da35dcb60463900476dcf
Maboela Maserame <maboelamaserame@gmail.com>
2023-04-10 18:06:19 +0200

    fix form section selector

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector(".hamburger-menu");
 const close = document.querySelector(".close-icon");
 const workLink = document.querySelector(".work-link");
 const  aboutLink= document.querySelector(".about-link");
-const contactLink= document.querySelector(".contact-link");
+const contactLink= document.querySelector(".contact");
 menu.onclick = function () {
 document.querySelector(".mobile-menu").classList.remove("hide-menu");
 document.querySelector(".mobile-menu").classList.add("show-menu");
