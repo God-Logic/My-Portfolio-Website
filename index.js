4c70cb6e04ceb891fb4e790fb0a3d9bc114038fe
Maboela Maserame <maboelamaserame@gmail.com>
2023-04-10 18:41:45 +0200

    add .eslintrc.jason file and fix linter error.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-window.onload= function () {
-const menu = document.querySelector(".hamburger-menu");
-const close = document.querySelector(".close-icon");
-const workLink = document.querySelector(".work-link");
-const  aboutLink= document.querySelector(".about-link");
-const contactLink= document.querySelector(".contact");
-menu.onclick = function () {
-document.querySelector(".mobile-menu").classList.remove("hide-menu");
-document.querySelector(".mobile-menu").classList.add("show-menu");
-}
-close.onclick = function () {
-  document.querySelector(".mobile-menu").classList.add("hide-menu");
+window.onload = () => {
+  const menu = document.querySelector('.hamburger-menu');
+  const close = document.querySelector('.close-icon');
+  const workLink = document.querySelector('.work-link');
+  const aboutLink = document.querySelector('.about-link');
+  const contactLink = document.querySelector('.contact');
+  menu.onclick = () => {
+    document.querySelector('.mobile-menu').classList.remove('hide-menu');
+    document.querySelector('.mobile-menu').classList.add('show-menu');
+  };
+  close.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  workLink.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  aboutLink.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  contactLink.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
 };
-workLink.onclick = function () {
-  document.querySelector(".mobile-menu").classList.add("hide-menu");
-};
-aboutLink.onclick = function () {
-  document.querySelector(".mobile-menu").classList.add("hide-menu");
-};
-contactLink.onclick = function () {
-  document.querySelector(".mobile-menu").classList.add("hide-menu");
-};
-}
