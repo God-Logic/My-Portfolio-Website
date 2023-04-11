9f1fe692676ff88bf21332ccbd195e7b58a7673b
nass <cabliveer@gmail.com>
2023-04-11 12:41:04 +0200

    Try to create dynamically using the information stored

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
-window.onload = () => {
-  const menu = document.querySelector('.hamburger-menu');
-  const close = document.querySelector('.close-icon');
-  const workLink = document.querySelector('.work-link');
-  const aboutLink = document.querySelector('.about-link');
-  const contactLink = document.querySelector('.contact');
-  menu.onclick = () => {
-    document.querySelector('.mobile-menu').classList.remove('hide-menu');
-    document.querySelector('.mobile-menu').classList.add('show-menu');
-  };
-  close.onclick = () => {
-    document.querySelector('.mobile-menu').classList.add('hide-menu');
-  };
-  workLink.onclick = () => {
-    document.querySelector('.mobile-menu').classList.add('hide-menu');
-  };
-  aboutLink.onclick = () => {
-    document.querySelector('.mobile-menu').classList.add('hide-menu');
-  };
-  contactLink.onclick = () => {
-    document.querySelector('.mobile-menu').classList.add('hide-menu');
-  };
-};
-
 const datas = [
-  {
-    image: "images/yoga-image.png",
-    name: "Multi-Post Stories",
-    description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    technologies: ["css", "html", "bootstrap", "Ruby"],
-    live: "https://god-logic.github.io/My-Portfolio-Website/",
-    source: "https://github.com/God-Logic/My-Portfolio-Website",
-  },
+  // {
+  //   image: "images/yoga-image.png",
+  //   name: "Multi-Post Stories",
+  //   description:
+  //     "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  //   technologies: ["css", "html", "bootstrap", "Ruby"],
+  //   live: "https://god-logic.github.io/My-Portfolio-Website/",
+  //   source: "https://github.com/God-Logic/My-Portfolio-Website",
+  // },
   {
     image: "images/Card 4.png",
     name: "Profesional Art Printing Data More",
@@ -87,3 +63,58 @@ const datas = [
     source: "https://github.com/God-Logic/My-Portfolio-Website",
   },
 ];
+
+
+window.onload = () => {
+  const menu = document.querySelector('.hamburger-menu');
+  const close = document.querySelector('.close-icon');
+  const workLink = document.querySelector('.work-link');
+  const aboutLink = document.querySelector('.about-link');
+  const contactLink = document.querySelector('.contact');
+  menu.onclick = () => {
+    document.querySelector('.mobile-menu').classList.remove('hide-menu');
+    document.querySelector('.mobile-menu').classList.add('show-menu');
+  };
+  close.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  workLink.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  aboutLink.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  contactLink.onclick = () => {
+    document.querySelector('.mobile-menu').classList.add('hide-menu');
+  };
+  
+  // const card = (data) => {
+  //   return `<img src=${data.image} alt="yoga illustration" class = image_one>
+    
+  //   <div class="multi-post">
+  //       <h3>${data.name}</h3>
+  //       <p>${data.description}</p>
+  //       <ul>
+  //         <li>${data.technologies[0]}</li>
+  //         <li>${data.technologies[1]}</li>
+  //         <li>${data.technologies[2]}</li>
+  //       </ul>
+  //       <button>See Project</button>
+  //   </div>`
+  // }
+
+  const card = (data) => {
+    return ``
+  }
+
+  // console.log(document.querySelector('.middle-part'))
+  document.querySelector('.middle-part').innerHTML = datas.map(data => card(data)).join("")
+
+
+  
+
+
+
+
+};
+
